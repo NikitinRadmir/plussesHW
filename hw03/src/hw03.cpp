fb797826bc13d312c044c01c338f3300017158df
ВашNikitinRadmirgit config --global user.email RANikitin@stud.kpfu.rugit config --global user.password ваш_ÐnrA2284856 <radmir.nikitin8@gmail.com>
2024-12-02 17:56:43 +0300

    1

--- hw03/src/hw03.cpp
+++ hw03/src/hw03.cpp
@@ -1,6 +1,24 @@
 #include <hw03.h>
+#include<iostream>
+#include<cstring>
+using namespace std;
+void hw03(char* str, const char* pattern) {
+  if (str == nullptr || pattern == nullptr) {
+    return;
+  }
 
-void hw03(char* str, const char* pattern)
-{
-  // Write code here..
+  size_t str_len = strlen(str);
+  size_t pattern_len = strlen(pattern);
+
+  if (str_len == 0 || pattern_len == 0 || pattern_len > str_len) {
+    return;
+  }
+
+  char* found = strstr(str, pattern);
+  if (found == nullptr) {
+    return;
+  }
+
+  // Перемещаем часть строки после найденной подстроки на длину подстроки влево
+  memmove(found, found + pattern_len, str_len - (found - str) - pattern_len + 1);
 }
